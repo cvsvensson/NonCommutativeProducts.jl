886eee6e297bc835e511669ac0418d4ea73f3a57
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2025-08-16 15:39:39 +0200

    add NCExp to handle exponents

--- src/sorting.jl
+++ src/sorting.jl
@@ -5,12 +5,16 @@ end
 scalar(s::ScalarMul) = s.λ
 
 struct AddTerm end
-# struct Power{S}
-#     sym::S
-#     exponent::Int
-# end
-# struct Nilpotent end
-# self_product_effect_type(::Fermion) = Nilpotent()
+struct NCExp{S}
+    term::S
+    exp::Int
+end
+Base.show(io::IO, e::NCExp) = print(io, "$(e.term)^$(e.exp)")
+
+
+struct Nilpotent end
+struct MaybeSwap end
+self_product_effect_type(::Fermion) = Nilpotent()
 
 should_swap(a, b, ::NaiveOrdering) = false
 should_swap(a::Fermion, b::Fermion, ::NormalOrdering) = !a.creation && b.creation
@@ -32,29 +36,84 @@ function bubble_sort(a::NCAdd, ordering; start=1)
     end
     return c
 end
+struct NCExpEffect{E}
+    effect::E
+end
+function mul_effect(a::NCExp, b::NCExp)
+    term_effect = mul_effect(a.term, b.term)
+    if term_effect isa NCExp
+        return NCExp(a.term, a.exp + b.exp)
+    elseif term_effect isa MaybeSwap
+        return MaybeSwap()
+    else
+        return NCExpEffect(term_effect)
+    end
+end
+function mul_effect(a::NCExp, b)
+    term_effect = mul_effect(a.term, b)
+    if term_effect isa NCExp
+        return NCExp(a.term, a.exp + 1)
+    elseif term_effect isa MaybeSwap
+        return MaybeSwap()
+    else
+        return NCExpEffect(term_effect)
+    end
+end
+function mul_effect(a, b::NCExp)
+    term_effect = mul_effect(a, b.term)
+    if term_effect isa NCExp
+        return NCExp(b.term, b.exp + 1)
+    elseif term_effect isa MaybeSwap
+        return MaybeSwap()
+    else
+        return NCExpEffect(term_effect)
+    end
+end
+should_swap(a::NCExp, b, ordering) = should_swap(a.term, b, ordering)
+should_swap(a, b::NCExp, ordering) = should_swap(a, b.term, ordering)
+should_swap(a::NCExp, b::NCExp, ordering) = should_swap(a.term, b.term, ordering)
+function swap_effect(a::NCExp, b::NCExp)
+    term_effect = swap_effect(a.term, b.term)
+    if term_effect isa ScalarMul
+        return ScalarMul(term_effect.λ^(a.exp * b.exp))
+    else
+        throw(ArgumentError("Don't know how to swap $a and $b"))
+    end
+end
+swap_effect(a::NCExp, b) = swap_effect(a, NCExp(b, 1))
+swap_effect(a, b::NCExp) = swap_effect(NCExp(a, 1), b)
 
 function bubble_sort(ncmul::NCMul, ordering; start=1)
     if length(ncmul.factors) == 1
         return ncmul
     end
     # muloraddvec::Union{Number,MulAdd} = a
-    newterms = []
+    # newterms = []
     swapped = false
     i = max(0, start - 1)
-    triple_prod(a, b, c) = ordered_product(ordered_product(a, b, NaiveOrdering()), c, NaiveOrdering())
+    # triple_prod(a, b, c) = ordered_product(ordered_product(a, b, NaiveOrdering()), c, NaiveOrdering())
     while !swapped && i < length(eachindex(ncmul.factors)) - 1
         i += 1
         a, b = ncmul.factors[i], ncmul.factors[i+1]
-        if should_swap(a, b, ordering)
+        effect = mul_effect(a, b)
+        if effect isa NCMul
             swapped = true
-            # left_factors = NCMul(ncmul.coeff, ncmul.factors[1:i-1])
-            # right_factors = NCMul(1, ncmul.factors[i+2:end])
-            effect = swap_effect(a, b)
-            if effect isa ScalarMul
-                ncmul = NCMul(scalar(effect) * ncmul.coeff, vcat(ncmul.factors[1:i-1], [b, a], ncmul.factors[i+2:end]))
+            ncmul = NCMul(ncmul.coeff, vcat(ncmul.factors[1:i-1], effect.factors, ncmul.factors[i+2:end]))
+        elseif effect isa NCExp
+            swapped = true
+            ncmul = NCMul(ncmul.coeff, vcat(ncmul.factors[1:i-1], [effect], ncmul.factors[i+2:end]))
+        elseif effect isa MaybeSwap
+            if should_swap(a, b, ordering)
+                swapped = true
+                effect = swap_effect(a, b)
+                if effect isa ScalarMul
+                    ncmul = NCMul(scalar(effect) * ncmul.coeff, vcat(ncmul.factors[1:i-1], [b, a], ncmul.factors[i+2:end]))
+                end
+                # product = ordered_product(ncmul.factors[i], ncmul.factors[i+1], ordering)
+                # muloraddvec = triple_prod(left_factors, product, right_factors)
             end
-            # product = ordered_product(ncmul.factors[i], ncmul.factors[i+1], ordering)
-            # muloraddvec = triple_prod(left_factors, product, right_factors)
+        elseif !isnothing(effect)
+            throw(ArgumentError("Don't know how to multiply $a * $b"))
         end
     end
     if !swapped
@@ -66,18 +125,20 @@ end
 # normal_order(a::MulAdd) = bubble_sort(a)
 # normal_order(a::Number) = a
 bubble_sort(a::Number, ordering; kwargs...) = a
-
+function mul_effect end
 
 @testitem "Signed permutation" begin
     using NonCommutativeProducts
-    import NonCommutativeProducts: ordered_product, NaiveOrdering, should_swap, NCMul, NCAdd, bubble_sort, swap_effect, ScalarMul
+    import NonCommutativeProducts: ordered_product, NaiveOrdering, should_swap, NCMul, NCAdd, bubble_sort, swap_effect, ScalarMul, NCExp, MaybeSwap, mul_effect
 
     struct NCInt
         n::Int
     end
-    Base.:==(a::NCInt, b::NCInt) = a.n == b.n
+    Base.show(io::IO, x::NCInt) = print(io, "̲$(x.n)")
+    Base.:(==)(a::NCInt, b::NCInt) = a.n == b.n
     struct IntOrder end
     Base.:*(a::NCInt, b::NCInt) = NCMul(1, [a, b])
+    mul_effect(a::NCInt, b::NCInt) = (a == b && return NCExp(a, 2)) || return MaybeSwap()
     should_swap(a::NCInt, b::NCInt, ::IntOrder) = a.n > b.n
     swap_effect(::NCInt, ::NCInt) = ScalarMul(-1)
 
@@ -85,7 +146,8 @@ bubble_sort(a::Number, ordering; kwargs...) = a
     a, b, c, d = NCInt.(1:4)
     ab = ordered_product(a, b, NaiveOrdering())
     @test ab == NCMul(1, [a, b])
-    @test bubble_sort(ab, IntOrder()) == ab
-    @test bubble_sort(ab * a, IntOrder()) == -1 * (a * ab)
+    @test bubble_sort(ab, IntOrder()) == ab == -bubble_sort(b * a, IntOrder())
+    @test bubble_sort(ab * a, IntOrder()) == bubble_sort(-1 * (a * ab), IntOrder())
 
+    bubble_sort(prod(rand((a, b, d, c)) for k in 1:10), IntOrder())
 end
