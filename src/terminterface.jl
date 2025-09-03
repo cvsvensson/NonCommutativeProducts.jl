2d12b520532a8b794df2ea78f9ed3d7049239462
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2025-09-03 11:52:40 +0200

    clean up terminterface.jl

--- src/terminterface.jl
+++ src/terminterface.jl
@@ -1,20 +1,15 @@
-using TermInterface
-
-# For NCAdd (non-commutative addition)
 TermInterface.isexpr(::NCAdd) = true
 TermInterface.iscall(::NCAdd) = true
 TermInterface.head(::NCAdd) = :call
 TermInterface.operation(::NCAdd) = +
 TermInterface.children(x::NCAdd) = arguments(x)
 
-# For NCMul (non-commutative multiplication)  
 TermInterface.isexpr(::NCMul) = true
 TermInterface.iscall(::NCMul) = true
 TermInterface.head(::NCMul) = :call
 TermInterface.operation(::NCMul) = *
 TermInterface.children(x::NCMul) = arguments(x)
 
-##
 function TermInterface.arguments(x::NCAdd{C1,NCMul{C2,T,F}}) where {C1,C2,T,F}
     C = promote_type(C1, C2)
     args = NCMul{C,T,F}[NCMul(C(x.coeff), T[])]
