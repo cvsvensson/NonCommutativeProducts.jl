b0f2c1d9d6778e45c4dadd66af4e06853a6abc35
William Samuelson <william.samuelson@gmail.com>
2026-05-08 13:43:42 +0200

    tests on mixed expression

--- src/traversal.jl
+++ src/traversal.jl
@@ -11,7 +11,7 @@ ncmap(f, x::NCMul) = prod(f(factor) for factor in x.factors; init=prefactor(x))
 ncmap(f, x::NCAdd) = sum(ncmap(f, term) for term in NCterms(x); init=additive_coeff(x))
 ncmap(f, x::Number) = x
 
-@testitem "ncmap" setup = [Fermions, Majoranas] begin
+@testitem "ncmap" setup = [Fermions, Majoranas, Bosons] begin
     import NonCommutativeProducts: ncmap
     NonCommutativeProducts.disable_autosort!()
     f = Fermion(:a)
@@ -24,16 +24,19 @@ ncmap(f, x::Number) = x
     @test ncmap(change_label, expr) == expected
     @test ncmap(identity, expr) == expr
 
-    scale_fermion(x::Fermion) = 2 * x
-    expected = 3 - 8 * f + 8 * f' * f
-    @test ncmap(scale_fermion, expr) == expected
+    scale_fermion_and_boson(x::Fermion) = 2 * x
+    scale_fermion_and_boson(x::Boson) = 3 * x
+    b = Boson()
+    expr = 3 - 4 * f + 2 * f' * b * f + b
+    expected = 3 - 8 * f + 24 * f' * b * f + 3 * b
+    @test ncmap(scale_fermion_and_boson, expr) == expected
 
     γ = Majorana.(1:2)
     ferm_to_maj(x::Fermion) = x.creation ? γ[1] - im * γ[2] : γ[1] + im * γ[2]
     ferm_to_maj(x::Majorana) = x
-    expr2 = 1 + f + 2 * f' * f + γ[1]
-    expected2 = 1 + (γ[1] + im * γ[2]) + 2 * (γ[1] - im * γ[2]) * (γ[1] + im * γ[2]) + γ[1]
-    @test ncmap(ferm_to_maj, expr2) == expected2
+    expr = 1 + f + 2 * f' * f + γ[1]
+    expected = 1 + (γ[1] + im * γ[2]) + 2 * (γ[1] - im * γ[2]) * (γ[1] + im * γ[2]) + γ[1]
+    @test ncmap(ferm_to_maj, expr) == expected
 
     @test ncmap(adjoint, f'f) == f * f'
     NonCommutativeProducts.enable_autosort!()
