738fac7feacb26dca80450d57f6bf27d0d86d414
Viktor Svensson <13223396+cvsvensson@users.noreply.github.com>
2026-05-19 13:07:43 +0200

    add benchmarks for mixed products

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,3 +11,11 @@ SUITE["symbolic_sum_square"] = @benchmarkable sum(Fermion(n)' * Fermion(n) + Fer
 
 labels = shuffle(1:10)
 SUITE["symbolic_deep_product"] = @benchmarkable prod(Fermion(l) for l in labels) * prod(Fermion(l)' for l in labels)
+
+include("../test/rules/bosons.jl")
+NonCommutativeProducts.@commutative Boson Fermion
+SUITE["symbolic_sum_mixed"] = @benchmarkable sum(Fermion(n)' * Fermion(n) + Fermion(n) * Fermion(n + 1)' + Boson(n)' * Boson(n + 1) for n in 1:1000)
+SUITE["symbolic_sum_square_mixed"] = @benchmarkable sum(Fermion(n)' * Fermion(n) + Fermion(n) * Fermion(n + 1)' + Boson(n)' * Boson(n + 1) for n in 1:10)^3
+
+labels = shuffle(1:6)
+SUITE["symbolic_deep_product_mixed"] = @benchmarkable prod(Fermion(l) for l in labels)* prod(Boson(l) for l in labels) * prod(Fermion(l)' for l in labels)
